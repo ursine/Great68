d9cacd5f592549ba0be3915d36286b731737e692
Gary Coulbourne <bear@bears.org>
2015-06-06 10:52:56 -0400

    start processing cmd line arguments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("Those Darn Geeks");
+    QCoreApplication::setOrganizationDomain("thosedarngeeks.com");
+    QCoreApplication::setApplicationName("Great68");
+    QCoreApplication::setApplicationVersion("0.1");
 
-    return a.exec();
+    QCommandLineParser parser;
+    parser.setApplicationDescription("An emulator for the Tandy Color Computers and accessories");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    // A boolean option with multiple values (-v)
+    QCommandLineOption vbs ( QStringList() << "v" << "verbose",
+                QCoreApplication::translate("main", "Show verbose logging about the system") );
+    parser.addOption(vbs);
+
+    // Process the actual command line arguments given by the user
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+
+    const bool verbose = parser.isSet(vbs);
+
+
+
+
+
+    return app.exec();
 }
