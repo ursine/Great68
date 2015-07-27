e209f42bc0c9c22b9c74e9504e24cb7dffa34a31
Gary Coulbourne <bear@bears.org>
2015-07-27 11:35:38 -0400

    opcode sorts

--- opcodes/code_maker.py
+++ opcodes/code_maker.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import re
+
 mc6809 = []
 hc6309 = []
 
@@ -12,9 +14,17 @@ def main():
             if line_stripped.startswith('#'):
                 continue
 
+            if not line_stripped:
+                continue
+
             hconly = line_stripped.startswith('*')
             if hconly:
                 line_stripped = line_stripped[1:].strip()
+
+            opnum = re.compile(r"([\d|A|B|C|D|E|F]+)\s+(\w+|/)\s+")
+            numm = opnum.search(line_stripped)
+
+            print numm.group()
 
             print line_stripped
 
