cad874852870865f8dff4252147ed410416e567e
Gary Coulbourne <bear@bears.org>
2015-09-06 19:23:20 -0400

    added basic address code

--- opcodes/code_maker.py
+++ opcodes/code_maker.py
@@ -2,12 +2,16 @@
 
 import re
 
-mc6809 = []
-hc6309 = []
+mc6809 = {}
+hc6309 = {}
+
+mc6809_10 = {}
+hc6309_10 = {}
+
+mc6809_11 = {}
+hc6309_11 = {}
 
 def main():
-
-    print "switch(opcode) {"
 
     with file('6309Opcodes.txt', 'r') as the_file:
         for the_line in the_file:
@@ -28,9 +32,31 @@ def main():
             numm = opnum.search(line_stripped)
 
             opcode = int(numm.group(1),16)
+            opname = numm.group(2)
 
             if opcode <= 0xFF:
-                print "    case 0x%0.2X : break;" % opcode
+                if not hconly:
+                    mc6809[opcode] = (opcode, opname)
+                hc6309[opcode] = (opcode, opname)
+
+    print "switch(opcode) {"
+    print
+
+    for opcode in xrange(0,0xFF):
+        opstr = "0x%0.2X" % opcode
+
+        support = "Unknown"
+        opname = "INVALID"
+        if (opcode in mc6809):
+            support = "Unsupported"
+            opname = mc6809[opcode][1]
+
+        print
+        print "    case %s : { // %s " % (opstr,opname)
+        print "        abort(\"%s opcode %s\");" % (support,opstr)
+        print "        break;"
+        print "    }"
+
 
 
     print "    case default: {"
--- opcodes/cpu.hpp
+++ opcodes/cpu.hpp
@@ -5,17 +5,17 @@
 
 constexpr size_t SIZE_64K = 1024*64;
 
+template<typename T>
 class Memory
 {
-  virtual uint8_t   read8(const uint16_t address) const = 0;
-  virtual uint16_t read16(const uint16_t address) const = 0;
-  
-  virtual void     write8(const uint16_t address, const uint8_t value)  = 0;
-  virtual void    write16(const uint16_t address, const uint16_t value) = 0;
+  virtual uint8_t   read8(const T address) const = 0;
+  virtual uint16_t read16(const T address) const = 0;
+
+  virtual void     write8(const T address, const uint8_t value)  = 0;
+  virtual void    write16(const T address, const uint16_t value) = 0;
 };
 
-template<size_t T>
-class SimpleRAM : public Memory
+class SimpleRAM : public Memory<uint16_t>
 {
 public:
   SimpleRAM() : Memory()
@@ -23,11 +23,17 @@ public:
     ram.fill(0);
   }
 
-  uint8_t read8(const uint16_t address) const { return ram.at(address); }
-  
-  
+  uint8_t  read8(const uint16_t address)  const override { return ram.at(address); }
+  uint16_t read16(const uint16_t address) const override { return ram.at(address) << 8 | ram.at(address+1); }
+
+  void write8(const uint16_t address, const uint8_t value) override { ram.at(address) = value; }
+  void write16(const uint16_t address, const uint16_t value) override {
+    ram.at(address)   = (value>>8) & 0xFF;
+    ram.at(address+1) = value & 0xFF;
+  }
+
 private:
-  std::array<uint8_t,T> ram;
+  std::array<uint8_t,SIZE_64K> ram;
 };
 
 class CPU
