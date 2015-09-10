04ccc8ee5abf489827d269818987626eba6d06fb
Gary Coulbourne <bear@bears.org>
2015-09-10 16:20:57 -0400

    read buffer

--- opcodes/cpu.cpp
+++ opcodes/cpu.cpp
@@ -1,8 +1,19 @@
 #include "cpu.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
 
 SimpleRAM::SimpleRAM() : Memory() {}
 
 void SimpleRAM::loadROM(const std::string& file, const uint16_t address)
 {
+  std::ifstream inf(file , std::ios::binary | std::ios::in );
 
+  auto start = std::istreambuf_iterator<char>(inf);
+  auto end   = std::istreambuf_iterator<char>();
+
+  std::vector<char> val(start,end);
+
+  std::copy(val.begin(), val.end(), ram.begin() + address);
 }
