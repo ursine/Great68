f47422677859a69777c556d5940325adbcc6c47c
Gary Coulbourne <gcoulbourne@trafficcast.com>
2019-02-12 14:41:20 -0500

    starting into lexing

--- asm6809.py
+++ asm6809.py
@@ -2,8 +2,17 @@
 
 import logging
 import sys
+from enum import Enum, auto
+
 
 logger = logging.getLogger('6809asm')
+
+
+class Tokens(Enum):
+    COMMENT = auto()
+    LABEL = auto()
+
+
 
 if __name__=='__main__':
     logging.basicConfig(level=logging.DEBUG)
@@ -18,7 +27,25 @@ if __name__=='__main__':
     logger.info(f"Total lines: {len(all_lines)}")
 
     # Pass One, convert to ASL
+    symbol_table = {}
 
-    for line in all_lines:
-        print(line)
+    for line, text in enumerate(all_lines):
+        lineno = 1+line
 
+        parts = []
+
+        if text.strip() == '':
+            continue # This is a blank line
+
+        if text[0] == '*':
+            continue # This is a comment
+
+        pieces = text.split()
+
+        if text[0] != ' ' and text[0] != '\t':
+            parts.append((Tokens.LABEL, pieces[0]))
+        
+        print(lineno, '-', parts, ':', text)
+
+
+        
